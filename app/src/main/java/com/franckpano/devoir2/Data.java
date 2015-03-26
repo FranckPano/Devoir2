43ebadfc73d014cd86a825d9896f61cea427949e
FranckPano <franck.pano@gmail.com>
2015-03-26 09:03:09 -0400

    D'où ça marche pas je vais me vénère
    YEEEEEAH

--- app/src/main/java/com/franckpano/devoir2/Data.java
+++ app/src/main/java/com/franckpano/devoir2/Data.java
@@ -5,7 +5,7 @@ package com.franckpano.devoir2;
  */
 public class Data {
         private long id;
-        private String comment;
+        private String name;
 
         public long getId() {
             return id;
@@ -14,17 +14,17 @@ public class Data {
         public void setId(long id) {
             this.id = id;
         }
-        public String getData() {
-            return comment;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setData(String comment) {
-            this.comment = comment;
-        }
+    public void setName(String content) {
+        this.name= content;
+    }
 
         // Will be used by the ArrayAdapter in the ListView
         @Override
         public String toString() {
-            return comment;
+            return name;
         }
 }
